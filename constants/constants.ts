f8e9fd1d4688443b8e7bb1f16436dc5e846f631e
dev-mohammadrezaahmadi <dev.mohammadrezaahmadi@outlook.com>
2023-09-25 01:11:53 +0330

    feat: add movie constant value

---
+++ constants/constants.ts
@@ -0,0 +1,7 @@
+// Sizes: w300, w780, w1280, original
+export const BACKDROP_SIZE: string = "w1280";
+
+// w92, w154, w185, w342, w500, w780, original
+export const POSTER_SIZE: string = "original";
+
+export const IMAGE_BASE_URL: string = "https://image.tmdb.org/t/p/";

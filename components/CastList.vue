1f8fdf948465a1052a77fdefbd67a7b41ec11079
dev-mohammadrezaahmadi <dev.mohammadrezaahmadi@outlook.com>
2023-09-25 01:12:49 +0330

    feat: add more details about cast of movie

---
+++ components/CastList.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+defineProps<{
+  cast: any[];
+}>();
+</script>
+
+<template>
+  <div class="w-full p-10 relative">
+    <h2 class="font-bold text-3xl uppercase py-5">cast</h2>
+    <div class="flex  overflow-x-scroll w-full space-x-4 scrollbar-hide">
+      <div v-for="actor in cast">
+        <CastThumbnail
+          class="border border-green-500 rounded-md"
+          :actor="actor"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ components/CastThumbnail.vue
@@ -0,0 +1,35 @@
+<script lang="ts" setup>
+defineProps<{
+  actor: {
+    profile_path: string;
+    name: string;
+    character: string;
+  };
+}>();
+
+const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/";
+const POSTER_SIZE = "w500";
+</script>
+
+<template>
+  <div
+    :style="{ background: 'rgba(0, 0, 0, 0.5)' }"
+    class="rounded-md w-[200px] h-[300px] p-5"
+  >
+    <img
+      :src="
+        actor.profile_path
+          ? `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}`
+          : ``
+      "
+      alt="actorthumbnail"
+      class="rounded-full w-[150px] h-[150px] object-cover"
+    />
+    <div class="mt-5 text-center">
+      <span class="itext-lg block">{{ actor.name }}</span>
+      <i>as</i> <span class="font-bold">{{ actor.character }}</span>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>

38a37ebbb9a8a55c445842521c796c19a41c9459
dev-mohammadrezaahmadi <dev.mohammadrezaahmadi@outlook.com>
2023-09-24 23:47:36 +0330

    fix: add image domain error

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  image: {
+    domains: ['image.tmdb.org']
+  },
   modules: ["@nuxtjs/tailwindcss", "@nuxt/image"]
 })

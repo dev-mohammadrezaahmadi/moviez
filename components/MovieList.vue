b9874da3e9be89c82032faa1d4d35e38652daf3c
dev-mohammadrezaahmadi <dev.mohammadrezaahmadi@outlook.com>
2023-09-24 23:51:48 +0330

    feat: add movie list component

---
+++ components/MovieList.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+defineProps<{
+  results: any[]
+}>()
+// import categories from "~/routes/categories";
+// const {query} = useRoute();
+
+// const url = `https://api.themoviedb.org/3${
+//   categories[route.query.genre as string]?.url || categories["trending"].url
+// }`;
+
+// const {} = useAsyncData(({query}) => {
+//   const genre = query.genre
+
+
+// })
+
+</script>
+
+<template>
+  <div
+    className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center"
+  >
+    <Thumbnail v-for="item in results" :key="item.id" :result="item" />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ components/Thumbnail.vue
@@ -0,0 +1,41 @@
+<script lang="ts" setup>
+import { HandThumbUpIcon } from "@heroicons/vue/24/outline";
+defineProps<{
+  result: any;
+}>();
+
+const BASE_URL = "https://image.tmdb.org/t/p/original";
+</script>
+
+<template>
+  <NuxtLink :href="`/${result.id}`">
+    <div
+      class="group cursor-pointer p-2 transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50"
+    >
+      <NuxtImg
+        height="1080"
+        width="1920"
+        alt="movie poster thumbnail"
+        :src="`${BASE_URL}${result.backdrop_path || result.poster_path}`"
+      />
+      <div class="p-2">
+        <p class="truncate max-w-md">{{ result.overview }}</p>
+        <h2
+          class="mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold"
+        >
+          {{ result.title || result.original_name }}
+        </h2>
+        <p class="flex items-center opacity-0 group-hover:opacity-100">
+          <span v-if="result.media_type">{{ result.media_type }}</span>
+          <span>&nbsp;</span>
+          <span>{{ result.release_date || result.first_air_date }}</span>
+          <span>&nbsp;</span>
+          <HandThumbUpIcon class="h-5 mx-2" />
+          <span>{{ result.vote_count }}</span>
+        </p>
+      </div>
+    </div>
+  </NuxtLink>
+</template>
+
+<style scoped></style>

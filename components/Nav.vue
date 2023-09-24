5271d0dc5002a5e7eb9b538a3d462a6ae5f82ad1
dev-mohammadrezaahmadi <dev.mohammadrezaahmadi@outlook.com>
2023-09-24 23:51:32 +0330

    feat: update nav routes based on categories new changes

--- components/Nav.vue
+++ components/Nav.vue
@@ -9,7 +9,8 @@ import categories from "~/routes/categories";
     >
       <NuxtLink
         v-for="category in categories"
-        :to="`/?genre=${category.title.toLocaleLowerCase()}`"
+        :key="category.key"
+        :to="`/category/${category.key.toLocaleLowerCase()}`"
       >
         <h2
           class="last:pr-24 capitalize cursor-pointer transition duration-100 transform hover:scale-125 hover:text-white active:text-green-500"

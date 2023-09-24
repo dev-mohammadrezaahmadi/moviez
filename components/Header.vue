2122f3fa7c6c2b5fcc893958769bd42b85868edb
dev-mohammadrezaahmadi <dev.mohammadrezaahmadi@outlook.com>
2023-09-24 15:54:14 +0330

    feat: add header

---
+++ components/Header.vue
@@ -0,0 +1,49 @@
+<script lang="ts" setup>
+import {
+  UserIcon,
+  CheckBadgeIcon,
+  HomeIcon,
+  MagnifyingGlassIcon,
+  BoltIcon,
+  RectangleStackIcon,
+} from "@heroicons/vue/24/outline";
+</script>
+
+<template>
+  <header
+    class="flex flex-col sm:flex-row m-5 justify-between items-center h-auto bg-blue-950"
+  >
+    <div class="flex flex-grow justify-evenly max-w-2xl">
+      <NuxtLink href="/">
+        <HeaderItem title="home">
+          <HomeIcon />
+        </HeaderItem>
+      </NuxtLink>
+
+      <HeaderItem title="trending">
+        <BoltIcon  />
+      </HeaderItem>
+      <HeaderItem title="verified">
+        <CheckBadgeIcon />
+      </HeaderItem>
+      <HeaderItem title="collections">
+        <RectangleStackIcon />
+      </HeaderItem>
+      <HeaderItem title="search">
+        <MagnifyingGlassIcon />
+      </HeaderItem>
+      <HeaderItem title="account">
+        <UserIcon />
+      </HeaderItem>
+    </div>
+    <NuxtLink href="/">
+      <!-- <Image
+        src="/hulu-white.png"
+        width="{200}"
+        height="{100}"
+        alt="moviedb logo"
+        class="object-contain cursor-pointer"
+      /> -->
+    </NuxtLink>
+  </header>
+</template>
---
+++ components/HeaderItem.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+defineProps({
+  title: {
+    type: String,
+  },
+});
+</script>
+
+<template>
+  <div
+    class="flex flex-col items-center cursor-pointer group w-12 sm:w-20 hover:text-white"
+  >
+    <div class="h-8 w-8 mb-1 group-hover:animate-bounce">
+      <slot> </slot>
+    </div>
+
+    <p class="opacity-0 group-hover:opacity-100 uppercase tracking-widest">
+      {{ title }}
+    </p>
+  </div>
+</template>
